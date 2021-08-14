dd567a07c1fbd1a3c551bd78edb14e233fda068c
Lorna Kimani <kimanilorna@yahoo.com>
2021-08-14 15:14:05 +0300

    modify solution for the fibonacci problem

--- fibonacci.js
+++ fibonacci.js
@@ -2,9 +2,15 @@ function fibonacci(N){
    if(N <= 1){
        return N
    }else{
-       console.log(fibonacci(N-1) + fibonacci(N-2))
-       return fibonacci(N-1) + fibonacci(N-2)
+    let n1 = 0
+    let n2 = 1
+    for (let i = 1; i <= N; i++) {
+        console.log(n1);
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
    }
 }
 
-fibonacci(3)
+fibonacci(10)
